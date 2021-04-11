18b87573bb64973f7e2aecfdd7cedcbb89ef82a8
zhaozhanwei@360.cn <zhaozhanwei@360.cn>
2021-04-11 23:57:36 +0800

    test fm

--- deepctr/recurrent/fm.py
+++ deepctr/recurrent/fm.py
@@ -1,5 +1,17 @@
-from tensorflow.python.keras.layer import Layer
+from tensorflow.python.keras.layers import Layer
 class FM(Layer):
     """
     实现
     """
+    def __init__(self, **kwargs):
+        super(FM, self).__init__(kwargs)
+
+    def build(self, input_shape):
+        if len(input_shape)!=3:
+            raise ValueError("input shape should be 3")
+
+        super(FM, self).build(input_shape)
+
+    def call(self, inputs):
+
+        return
--- tests/feature_test.py
+++ tests/feature_test.py
@@ -1,12 +1,12 @@
 from deepctr.models import DeepFM
-from deepctr.feature_column import SparseFeat, DenseFeat,get_feature_names
+from deepctr.feature_column import SparseFeat, DenseFeat, get_feature_names
 import numpy as np
 def test_long_dense_vector():
 
     feature_columns = [SparseFeat('user_id', 4, ), SparseFeat('item_id', 5, ), DenseFeat("pic_vec", 5)]
     fixlen_feature_names = get_feature_names(feature_columns)
-
     user_id = np.array([[1], [0], [1]])
+
     item_id = np.array([[3], [2], [1]])
     pic_vec = np.array([[0.1, 0.5, 0.4, 0.3, 0.2], [0.1, 0.5, 0.4, 0.3, 0.2], [0.1, 0.5, 0.4, 0.3, 0.2]])
     label = np.array([1, 0, 1])
@@ -17,3 +17,5 @@ def test_long_dense_vector():
     model = DeepFM(feature_columns, feature_columns[:-1])
     model.compile('adagrad', 'binary_crossentropy')
     model.fit(model_input, label)
+
+test_long_dense_vector()
